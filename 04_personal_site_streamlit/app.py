acbe51192adc96741ae3c014eff656f7f8b450d1
yyy1619 <yao20010912@gmail.com>
2025-04-07 15:30:28 +0800

    Add files via upload

---
+++ 04_personal_site_streamlit/app.py
@@ -0,0 +1,5 @@
+import os
+os.environ['PYTHONUNBUFFERED'] = '1'
+import streamlit as st
+
+# Your Streamlit app code here
---
+++ 04_personal_site_streamlit/streamlit_app.py
@@ -0,0 +1,58 @@
+# %%
+import warnings
+import streamlit as st
+
+warnings.simplefilter(action="ignore", category=FutureWarning)
+
+# Must be the first Streamlit command
+st.set_page_config(page_title="yyy's site", layout="wide")
+
+# %%
+# Import pages from the new directory
+from page_content.home import home_page
+from page_content.experience import experience_page
+from page_content.education import education_page
+from page_content.resume import resume_page
+from page_content.contact import contact_page
+
+# Import components
+from components.footer import display_footer
+from components.styles import load_css
+
+class MultiApp:
+    def __init__(self):
+        self.apps = []
+
+    def add_app(self, title, func):
+        self.apps.append({"title": title, "function": func})
+
+    def run(self):
+        # Load custom CSS
+        load_css()
+
+        # 直接显示导航选项，移除了 "## Main Menu" 标题
+        app = st.sidebar.radio(
+            "", self.apps, format_func=lambda app: app["title"]
+        )
+        st.sidebar.markdown("---")
+
+        app["function"]()
+        
+        # Display footer at the bottom of each page
+        display_footer()
+
+# Initialize the app
+app = MultiApp()
+
+# Add pages to the app
+app.add_app("Home Page", home_page)
+app.add_app("Contact Page", contact_page)
+app.add_app("Education Page", education_page)
+app.add_app("Experience Page", experience_page)
+app.add_app("Resume Page", resume_page)
+
+
+# Run the app
+app.run()
+
+# %%
